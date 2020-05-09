e468c8294801d9c34bdb34df87bdcea9cef97926
Baktygul <bakulyakozhoshova@gmail.com>
2020-05-09 23:06:18 +0600

    some addition

--- src/main/java/com/myshop/online/controller/RestProductController.java
+++ src/main/java/com/myshop/online/controller/RestProductController.java
@@ -6,15 +6,13 @@ import com.myshop.online.model.Product;
 import com.myshop.online.repository.ProductRepository;
 import com.myshop.online.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@Controller
-@RequestMapping("/products")
-//@RestController
-public class ProductController {
+//@Controller
+@RequestMapping
+@RestController
+public class RestProductController {
     @Autowired
     private ProductService service;
     @Autowired
@@ -32,15 +30,15 @@ public class ProductController {
         return service.saveProducts(products);
     }
 
- /*   @GetMapping ("/products")
+    @GetMapping
     public List<Product> findAllProducts(){
         return service.getProducts();
-    }*/
+    }
 
-  /*  @GetMapping ("/idproduct/{id}")
+    @GetMapping ("/idproduct/{id}")
     public Product findProductById(@PathVariable int id){
         return  service.getProductById(id);
-    }*/
+    }
 
     @GetMapping ("/product/{name}")
     public Product findProductByName(@PathVariable String name){
