6eaab76a2f2475334656de9260036e1778359e74
Baktygul <bakulyakozhoshova@gmail.com>
2020-05-21 19:18:21 +0600

    language, translation

--- src/main/java/com/myshop/online/model/Order.java
+++ src/main/java/com/myshop/online/model/Order.java
@@ -3,6 +3,7 @@ package com.myshop.online.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -15,10 +16,32 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column
+    private Date orderDate;
+
+    @Column
+    private int orderNum;
+
+    @Column
+    private double amount;
+
+    @Column
+    private String customerName;
+
+    @Column
+    private String customerAddress;
+
+    @Column
+    private String customerEmail;
+
+    @Column
+    private String customerPhone;
 
     @OneToMany
     private List<ProductInCart> productInCartList;
 
+    public void setId(String toString) {
+    }
 }
 
 
